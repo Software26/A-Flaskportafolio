e8bf954159e99e3d87f2ba23bd35aa89c43f1736
Natanael Sosa <natanaelsosa@gmail.com>
2024-05-04 10:14:54 -0400

    add the templates

--- app.py
+++ app.py
@@ -5,6 +5,11 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template("index.html")
+
+@app.route('/proyect')
+def proyect():
+    return render_template("portafolio.html")
+    
     
 if __name__ == '__main__':
     app.run(debug=True)
