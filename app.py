69f040a8726cbeb06b2d37e6ca19a6113b959bb8
Natanael Sosa <natanaelsosa@gmail.com>
2024-05-05 14:54:34 -0400

    Send_mail configuration

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template
-from flask_mail import Mail
+from flask import Flask, render_template, request, redirect,url_for
+from flask_mail import Mail, Message
 app = Flask(__name__)
 
 app.config['MAIL_SERVER']='sandbox.smtp.mailtrap.io'
@@ -19,8 +19,22 @@ def index():
 def proyect():
     return render_template("portafolio.html")
     
-@app.route('/mail_mail')
+@app.route('/mail', methods=['GET','POST'])
 def send_mail():
-    return render_template('send_mail.html')
+    if request.method == 'POST':
+        name = request.form['name']
+        email = request.form['email']
+        message = request.form.get('message')
+
+        msg = Message('Hola Natanael, tienes un nuevo contenido de la web:',
+                      body=f'Nombre: {name}\nCorreo: {email}\n\nEscribió:\n\n{message}',
+                      sender=email,
+                      recipients=['natanaelsosa@gmail.com']
+        )
+
+        mail.send(msg)
+        return render_template('send_mail.html')
+    return redirect(url_for('index'))
+
 if __name__ == '__main__':
     app.run(debug=True)
