078226801d2c4bdd174d8ab40588ed7ccb8cef3d
Natanael Sosa <natanaelsosa@gmail.com>
2024-05-05 13:09:59 -0400

    Configuration for mail

--- app.py
+++ app.py
@@ -1,6 +1,15 @@
 from flask import Flask, render_template
-
+from flask_mail import Mail
 app = Flask(__name__)
+
+app.config['MAIL_SERVER']='sandbox.smtp.mailtrap.io'
+app.config['MAIL_PORT'] = 2525
+app.config['MAIL_USERNAME'] = 'e8350258b9a19a'
+app.config['MAIL_PASSWORD'] = '5dfc23fd627920'
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USE_SSL'] = False
+
+mail = Mail(app)
 
 @app.route('/')
 def index():
@@ -10,7 +19,7 @@ def index():
 def proyect():
     return render_template("portafolio.html")
     
-@app.route('/mail')
+@app.route('/mail_mail')
 def send_mail():
     return render_template('send_mail.html')
 if __name__ == '__main__':
